8f2bf1308745489441ddf98cbdcccdae4b8c2e1f
chris59370 <heddebaut-christophe@bbox.fr>
2021-12-13 11:41:02 +0100

    modification du contenue

--- src/App.js
+++ src/App.js
@@ -15,20 +15,24 @@ function App() {
           j’ai travaillé en équipe sur plusieurs sites de nettoyage diffèrent.</p>
         <h2>Qualités:</h2>
         <ul>
-          <il>	Diplomatie : je parle calmement et fait preuve de recul sur la situation</il>
+          <il>&#8227;	Diplomatie : je parle calmement et fait preuve de recul sur la situation</il>
         </ul>
         <ul>
-          <il>	Attentif : si je dois faire quelque chose qui est demander c’est fait dans tout ou le lendemain selon la situation </il>
+          <il>&#8227;	Attentif : si je dois faire quelque chose qui est demander c’est fait tout de suite ou le lendemain selon la situation </il>
         </ul>
         <ul>
-          <il>	Consciencieux : durant mon travail les clients ma souvent dit que je suis consciencieux </il>
+          <il>&#8227;	Consciencieux : durant mon travail les clients ma souvent dit que je suis consciencieux </il>
         </ul>
         
         <ul>
-          <il>	Rapide et efficace : exemple je suis capable de nettoyer une salle du McDonald en 2H30 aux lieux de 3H </il>
+          <il>&#8227;	Rapide et efficace : exemple je suis capable de nettoyer une salle du McDonald en 2H30 aux lieux de 3H </il>
+        </ul>
+        <ul>
+          <il>&#8227;	Prendre des initiatives : voyant l’état du secteur j’ai décidé de faire une remise en état </il>
         </ul>
         <h2>Mon porjet</h2>
-        <p>Etant maintenant je souhaite mettre mes qualités au sein de l’entreprise qui 
+        <p>Ayant effectué deux formations en développement web et dotnet  
+          je souhaite mettre mes qualités au sein de l’entreprise qui 
           m’accepterai comme junior et me formerai au développement pour un stage 
           d’un mois du 11 janvier au O7 février 2022 en plus de mon auto formation, 
           afin de continuer dans ce domaine. </p>
