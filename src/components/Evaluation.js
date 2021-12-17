c2de4908e6bded03e82a7c9a1753ce7780f45708
chris59370 <heddebaut-christophe@bbox.fr>
2021-12-17 16:19:45 +0100

    finalisation du portfolio

--- src/components/Evaluation.js
+++ src/components/Evaluation.js
@@ -17,10 +17,171 @@ function Evaluation(){
           </ul>
           <ul>
           <li class="header-center slider" >
-            <img class="taille" src="img/1.jpg" alt="1"/>
+            <img className="taille" src="img/1.jpg" alt="1"/>
           </li>
         
         </ul>
+        <section>
+            <p className='nos'>Nos vehicules</p>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/1.jpg' alt='premiere voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                    <div className='prix-barrer'>
+                        91 000€
+                    </div>
+                    <div>
+                        79 000€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+            <hr/>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/2.jpg' alt='deuxieme voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                    
+                    <div>
+                        71 000€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+            <hr/>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/3.jpg' alt='troisieme voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                    
+                    <div>
+                        79 000€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+            <hr/>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/4.jpg' alt='quatrieme voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                <div className='prix-barrer'>
+                        91 000€
+                    </div>
+                    
+                    <div>
+                        79 000€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+            <hr/>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/5.jpg' alt='cinquieme voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                
+                    
+                    <div>
+                       50 995€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+            <hr/>
+            <div className='top'>
+                <div className='flotte'>
+                    <img className='voiture' src='products/6.jpg' alt='sixieme voiture'/>
+                <p className='droite'>
+                    <h2 className='position'>
+                        lorem ipsum dolor sit amet
+                    </h2>
+                    <p className='position1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Expedita porro amet a eum alias laborum ut explicabo,  
+                    </p>
+                </p>
+                </div>
+                <div className='prix'>
+                <div className='prix-barrer'>
+                        81 500€
+                    </div>
+                    
+                    <div>
+                       77 999€
+                    </div>
+                    <div>
+                    <button>+ COMPARER</button>
+                    </div>
+
+                </div>
+            </div>
+        </section>
         </div>
     )
 }export default Evaluation
